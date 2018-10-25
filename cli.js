8febc57d4b853f55002c039c52f898e440eb07a2
Antoni Kepinski <keppi@o2.pl>
2018-10-25 20:58:43 +0200

    init :smile:

---
+++ cli.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const meow = require('meow');
+const chalk = require('chalk');
+const got = require('got');
+const Ora = require('ora');
+
+const cli = meow(`
+	Usage
+	  $ weather <options>
+
+    Options
+      --location, -l    Specify location
+      --units, -u       Specify units (imperial or metrics)
+
+	Examples
+	  $ weather --location New York
+	  $ weather -l Prague -u imperial
+`, {
+	flags: {
+		location: {
+			type: 'string',
+			alias: 'l',
+			default: 'Tulce'
+		},
+		units: {
+			type: 'string',
+			alias: 'u',
+			default: 'metric'
+		}
+	}
+});
+
+const spinner = new Ora();
+
+spinner.start('Fetching data...');
+
+(async () => {
+	try {
+		const response = await got(`https://api.openweathermap.org/data/2.5/weather?q=${cli.flags.location}&units=${cli.flags.units}&APPID=a40636258ec257059436a5ac207bc5ac`, {json: true});
+
+		spinner.succeed('Weather report:\n');
+
+		console.log(`${chalk.green.bold(response.body.name)}\n`);
+
+		if (cli.flags.units === 'imperial') {
+			console.log(`Temperature: ${response.body.main.temp} Fahrenheit`);
+		} else {
+			console.log(`Temperature: ${response.body.main.temp} Celsius`);
+		}
+		console.log('--------');
+		console.log(`Pressure: ${response.body.main.pressure} HPa`);
+		console.log(`Humidity: ${response.body.main.humidity} %`);
+		console.log(`Cloudiness: ${response.body.clouds.all} %`);
+		console.log(`Wind speed: ${response.body.wind.speed} km/h\n`);
+	} catch (error) {
+		spinner.fail('Something went wrong!');
+	}
+})();
